767090305fb1e076b03c5e29476fd6f12cb645ee
BushraTariq1624 <tbushra211@gmail.com>
2025-04-26 22:55:54 +0500

    Attendance System

--- backend/index.mjs
+++ backend/index.mjs
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
 	cors({
-		origin: ['http://localhost:5173','https://attendance-system-flax.vercel.app/'],
+		origin: ['http://localhost:5173','https://attendance-system-flax.vercel.app'],
 		methods: ['GET', 'PUT', 'POST', 'DELETE'],
 		credentials: true,
 	}),
